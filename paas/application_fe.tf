8fdcb6e3de37ee3befc26cc20afadec96cddebf4
robhettrick <robert.hettrick@thepsc.co.uk>
2022-01-06 11:40:09 +0000

    tidy up...

--- paas/application_fe.tf
+++ paas/application_fe.tf
@@ -14,12 +14,19 @@ resource "cloudfoundry_app" "fe" {
     IRON_NAME               = var.paas_app_iron_name
     IRON_PASSWORD           = var.paas_app_iron_password
     PAGES_LOCATION          = "./.next/server/pages"
-    GRAPHQL_URL             = format("http://%s.apps.internal:8080/graphql", cloudfoundry_route.api_route_internal.hostname)
-    BASE_API_URL            = "https://${cloudfoundry_route.app_route_cloud.hostname}.${data.cloudfoundry_domain.cloudapps.name}/api"
+    GRAPHQL_URL             = "http://${cloudfoundry_route.api_route_internal.hostname}.${data.cloudfoundry_domain.internal.name}:8080/graphql"
+    BASE_API_URL            = <<-EOT
+    "${var.paas_custom_domain_flag == "false" ?
+    "https://${cloudfoundry_route.app_route_cloud.hostname}.${data.cloudfoundry_domain.cloudapps.name}/api" :
+    "https://var.paas_custom_subdomain.${data.cloudfoundry_domain.custom.name}/api"}"
+    EOT
   }
 
-  routes {
-    route = cloudfoundry_route.app_route_cloud.id
+  dynamic "routes" {
+    for_each = var.paas_custom_domain_flag? [] : [1]
+    content {
+      route = cloudfoundry_route.app_route_cloud.id
+    }
   }
 
   dynamic "routes" {
