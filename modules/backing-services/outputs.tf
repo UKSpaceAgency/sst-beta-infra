c552ef20927f780e99b382fec5063c395242c6e4
robhettrick <robert.hettrick@thepsc.co.uk>
2022-02-01 17:07:38 +0000

    add redis backing service...

--- modules/backing-services/outputs.tf
+++ modules/backing-services/outputs.tf
@@ -6,6 +6,10 @@ output "s3" {
   value = cloudfoundry_service_instance.aws_s3_bucket
 }
 
+output "redis" {
+  value = cloudfoundry_service_instance.redis
+}
+
 output "logit" {
   value = cloudfoundry_user_provided_service.logit
 }
