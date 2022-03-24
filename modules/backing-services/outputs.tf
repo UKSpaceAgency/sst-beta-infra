4832d68341c2302ce6ed8a339187c852be7d1616
robhettrick <robert.hettrick@thepsc.co.uk>
2022-03-24 17:41:13 +0000

    comment out redis whilst not in use...

--- modules/backing-services/outputs.tf
+++ modules/backing-services/outputs.tf
@@ -5,11 +5,11 @@ output "db" {
 output "s3" {
   value = cloudfoundry_service_instance.aws_s3_bucket
 }
-
+/*
 output "redis" {
   value = cloudfoundry_service_instance.redis
 }
-
+*/
 output "logit" {
   value = cloudfoundry_user_provided_service.logit
 }
