2ad18d14f04180113d477fc1f8c48acdfbf6d9a7
robhettrick <robert.hettrick@thepsc.co.uk>
2022-04-01 17:26:11 +0100

    revert api to paas domain temporarily...

--- modules/network-routes/main.tf
+++ modules/network-routes/main.tf
@@ -32,8 +32,12 @@ resource "cloudfoundry_route" "spacetrack" {
 }
 
 resource "cloudfoundry_route" "api" {
+  domain   = data.cloudfoundry_domain.cloudapps.id
+  hostname = "${ var.app_api_route_name }-${ var.env_tag }"
+  /*
   domain   = "${ var.env_tag == "prod"? data.cloudfoundry_domain.custom.id : data.cloudfoundry_domain.cloudapps.id}"
   hostname = "${ var.env_tag == "prod"? var.custom_api_subdomain : "${ var.app_api_route_name }-${ var.env_tag }" }"
+  */
   space    = var.space.id
 }
 
