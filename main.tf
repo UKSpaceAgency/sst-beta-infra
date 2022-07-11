2e179d9c7f7f3fc3f1d5d5b38e5e2784452c74de
Wojciech Gintowt <w.gintowt@adform.com>
2022-07-11 10:26:14 +0200

    adding meaningless new line

--- main.tf
+++ main.tf
@@ -70,6 +70,7 @@ module "back-end" {
   notifiers_webhook_url                     = var.notifiers_webhook_url
   app_sentry_dsn                            = var.app_sentry_dsn
   app_spacetrack_worker_sentry_dsn          = var.app_spacetrack_worker_sentry_dsn
+
 }
 
 module "front-end" {
