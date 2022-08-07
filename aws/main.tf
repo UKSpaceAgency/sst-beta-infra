9790989d43a03c6398665735b700c56667725efd
Wojciech Gintowt <w.gintowt@adform.com>
2022-08-07 11:49:01 +0200

    remove not used folder

--- aws/main.tf
+++
@@ -1,7 +0,0 @@
-
-module "public_route53_hosted_zone" {
-  source         = "./modules/public_route53_hosted_zone"
-  name           = "monitor-your-satellites.service.gov.uk"
-  product_domain = "mys-prod"
-  environment    = "prod"
-}
