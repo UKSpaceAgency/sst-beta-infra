c6673c5a8b238e91e327cce419b37dea5d768954
Wojciech Gintowt <w.gintowt@adform.com>
2022-08-16 14:53:25 +0200

    adding more to route53

--- aws/modules/public_route53_hosted_zone/variables.tf
+++ aws/modules/public_route53_hosted_zone/variables.tf
@@ -40,7 +40,13 @@ variable "dns_records_CNAME" {
   default     = {
       www-dev = "d16nxratuxxje8.cloudfront.net"
       "_238c0ef85e86d097a18604dd74ea1726.www-dev" = "_602c9f3f757601639b5599241beac658.vrztfgqhxj.acm-validations.aws."
+      dev = "d16nxratuxxje8.cloudfront.net"
+      "_6bd2aec10b1f378e33e6c9f0d5abc319.dev" = "_f5d5cfecc1a1cc00bb013db2204d4969.zxwlrjxpwn.acm-validations.aws."
       api-dev = "d10qurojk9ecxy.cloudfront.net"
       "_611464eb053bab8dbfc5c9f63ee5a5ce.api-dev" = "_25c48ef80fb5a21bcfc606956c99bb56.vrztfgqhxj.acm-validations.aws."
+      api-demo = "deifh0fuhtqpn.cloudfront.net"
+      "_9a607ca3dc863fc1da769224448f838f.api-demo" = "_c8cf621010a08f698ca8ec9714248b31.zxwlrjxpwn.acm-validations.aws."
+      demo = "d2d3pngzkm5msd.cloudfront.net"
+      "_13daf2d2cfb9c775577fb5a495076822.demo" = "_b05718b68e0513434750a5d135ed215d.zxwlrjxpwn.acm-validations.aws."
   }
 }
