aef8d1f6b4bebf3e92fb958c7131583be5f68b4b
Wojciech Gintowt <w.gintowt@adform.com>
2022-09-16 14:51:19 +0200

    adding more vars

--- modules/back-end/main.tf
+++ modules/back-end/main.tf
@@ -89,6 +89,9 @@ resource "cloudfoundry_app" "esa_discos" {
 
   environment = {
     APP_ENVIRONMENT                           = var.env_tag
+    ESA_DISCOS_ACCESS_TOKEN                   = var.esa_discos_access_token
+    NOTIFY_API_KEY                            = var.notify_api_key
+    NOTIFY_INTERVAL                           = var.notify_interval
     RUN_AT_HOUR                               = var.run_at_hour
     RUN_AT_MINUTE                             = var.run_at_minute
     APP_SENTRY_DSN                            = var.app_spacetrack_worker_sentry_dsn
