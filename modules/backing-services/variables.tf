e897f8ad0ba577821dfa4ffe47e23200f8e6d550
robhettrick <robert.hettrick@thepsc.co.uk>
2022-10-06 14:59:55 +0100

    temporarily disable pg_stat_statements...

--- modules/backing-services/variables.tf
+++ modules/backing-services/variables.tf
@@ -15,7 +15,7 @@ variable "db_plan" {
 }
 
 variable "db_extensions" {
-  //default = "{\"enable_extensions\": [\"pg_stat_statements\"]}"
+  default = "{\"enable_extensions\": [\"pg_stat_statements\"]}"
 }
 
 variable "s3_name" {
