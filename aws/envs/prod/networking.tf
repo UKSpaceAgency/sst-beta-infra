50d9c92433eee27c731e4db8584c59b97cdd6722
thepsc-go <gregory.o@thepsc.co.uk>
2023-08-04 08:41:53 +0200

    prod upgrade and networking

--- aws/envs/prod/networking.tf
+++ aws/envs/prod/networking.tf
@@ -1,7 +1,5 @@
 module "network" {
   source     = "../../tf-modules/networking"
   env_name   = var.env_name
-  cidr_block = "172.18.24.0/22"
-  private_subnet_bits = 4
-  public_subnet_bits = 4
+  cidr_block = "172.20.0.0/16"
 }
