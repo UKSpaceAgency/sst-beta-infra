dd09d9d4c0c6e477710d1fdd04af6042c95d9daf
thepsc-go <gregory.o@thepsc.co.uk>
2023-08-09 14:30:30 +0200

    turn off prod db for a while

---
+++ aws/envs/prod/db.tf
@@ -0,0 +1,8 @@
+#module "db" {
+#  source                 = "../../tf-modules/db"
+#  env_name               = var.env_name
+#  vpc_security_group_ids = [module.network.pg-security-group-id]
+#  db_subnet_ids          = module.network.private_subnet_ids
+#  instances_no           = 1
+#  max_acu = 64
+#}
