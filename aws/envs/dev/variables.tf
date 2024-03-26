29be7c09ec2e31a94c00541853f71bc524a82a91
thepsc-go <gregory.o@thepsc.co.uk>
2024-03-26 12:10:18 +0100

    mys2msh redirects

--- aws/envs/dev/variables.tf
+++ aws/envs/dev/variables.tf
@@ -8,6 +8,11 @@ variable "route53_domain" {
   default = "dev.monitor-space-hazards.service.gov.uk"
 }
 
+variable "mys_route53_domain" {
+  type    = string
+  default = "dev.monitor-your-satellites.service.gov.uk"
+}
+
 #variable "auth_client_id" {
 #  type    = string
 #  sensitive = true
