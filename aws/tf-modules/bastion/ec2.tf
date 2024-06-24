49cd575f7575c9f77c3a7e48fd6234e465ca1e1e
thepsc-go <gregory.o@thepsc.co.uk>
2024-06-24 15:45:26 +0200

    bastion not needed

--- aws/tf-modules/bastion/ec2.tf
+++ aws/tf-modules/bastion/ec2.tf
@@ -28,27 +28,27 @@ resource "aws_iam_instance_profile" "bastion_profile" {
   role = aws_iam_role.ec2_s3_full_access_role.name
 }
 
-resource "aws_instance" "bastion" {
-
-  instance_type          = "t2.micro"
-  ami                    = "ami-06464c878dbe46da4" //Amazon Linux 2023 AMI x86 64bit
-  key_name               = "go_keypair"
-  vpc_security_group_ids = var.vpc_security_group_ids
-  subnet_id              = var.public_subnet_id
-  iam_instance_profile   = aws_iam_instance_profile.bastion_profile.name
-  tags = {
-    Name = "bastion-host-for-${var.env_name}"
-  }
-
-  user_data = <<-EOF
-    #!/bin/bash
-    mkfs.ext4 /dev/sdf
-    mkdir /opt/data
-    mount /dev/sdf /opt/data
-    echo '/dev/sdf  /opt/data ext4 defaults 0 0' >> /etc/fstab
-    chown -R ec2-user:ec2-user /opt/data
-    yum install -y postgresql15
-    wget -O /etc/yum.repos.d/cloudfoundry-cli.repo https://packages.cloudfoundry.org/fedora/cloudfoundry-cli.repo
-    yum install -y cf8-cli
-    EOF
-}
+#resource "aws_instance" "bastion" {
+#
+#  instance_type          = "t2.micro"
+#  ami                    = "ami-06464c878dbe46da4" //Amazon Linux 2023 AMI x86 64bit
+#  key_name               = "go_keypair"
+#  vpc_security_group_ids = var.vpc_security_group_ids
+#  subnet_id              = var.public_subnet_id
+#  iam_instance_profile   = aws_iam_instance_profile.bastion_profile.name
+#  tags = {
+#    Name = "bastion-host-for-${var.env_name}"
+#  }
+#
+#  user_data = <<-EOF
+#    #!/bin/bash
+#    mkfs.ext4 /dev/sdf
+#    mkdir /opt/data
+#    mount /dev/sdf /opt/data
+#    echo '/dev/sdf  /opt/data ext4 defaults 0 0' >> /etc/fstab
+#    chown -R ec2-user:ec2-user /opt/data
+#    yum install -y postgresql15
+#    wget -O /etc/yum.repos.d/cloudfoundry-cli.repo https://packages.cloudfoundry.org/fedora/cloudfoundry-cli.repo
+#    yum install -y cf8-cli
+#    EOF
+#}
