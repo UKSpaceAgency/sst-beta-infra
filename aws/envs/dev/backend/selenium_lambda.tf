666077246690f1f1285c5d0b2cc5b1f0de381945
thepsc-go <gregory.o@thepsc.co.uk>
2024-10-29 16:20:09 +0100

    selenium lambda env vars and iam policies

--- aws/envs/dev/backend/selenium_lambda.tf
+++ aws/envs/dev/backend/selenium_lambda.tf
@@ -3,6 +3,39 @@ data "aws_region" "current" {}
 data "aws_caller_identity" "current" {}
 
 
+resource "aws_iam_policy" "lambda-iam-policy-selenium" {
+  name        = "iam-policy-for-selenium-lambda"
+  path        = "/"
+  description = "IAM policy for selenium lambda"
+
+  policy = jsonencode(
+    {
+      Version : "2012-10-17",
+      Statement : [
+        {
+          Action : [
+            "logs:CreateLogGroup",
+            "logs:CreateLogStream",
+            "logs:PutLogEvents"
+          ],
+          Resource : "arn:aws:logs:*:*:*",
+          Effect : "Allow"
+        },
+        {
+          "Action": [
+            "s3:*"
+          ],
+          "Effect": "Allow",
+          "Resource": [
+            data.terraform_remote_state.stack.outputs.s3_reentry_bucket_arn,
+            "${data.terraform_remote_state.stack.outputs.s3_reentry_bucket_arn}/*"
+          ]
+        }
+      ]
+    }
+  )
+}
+
 resource "aws_iam_role" "lambda-assume-role-selenium-lambda" {
   name = "iam-role-for-selenium-lambda"
 
@@ -18,6 +51,7 @@ resource "aws_iam_role" "lambda-assume-role-selenium-lambda" {
           Effect : "Allow",
           Sid : ""
         }
+
       ]
     }
   )
@@ -25,20 +59,34 @@ resource "aws_iam_role" "lambda-assume-role-selenium-lambda" {
 
 data "aws_ecr_image" "service_image" {
   repository_name = "selenium-lambda"
-  image_tag       = "latest"
+  image_tag       = var.image_tag
+}
+
+data "aws_secretsmanager_secret" "frontend_secret" {
+  name = "${var.env_name}-frontend"
+}
+
+data "aws_secretsmanager_secret_version" "frontend_secret_version" {
+  secret_id = data.aws_secretsmanager_secret.frontend_secret.id
+}
+
+locals {
+  secret_data = jsondecode(data.aws_secretsmanager_secret_version.frontend_secret_version.secret_string)
 }
 
 module "selenium_lambda" {
   source               = "../../../tf-modules/lambda_as_image"
   env_name             = var.env_name
   lambda_function_name = "selenium-lambda-${var.env_name}"
-  lambda_policy_arn    = data.terraform_remote_state.stack.outputs.lambda_iam_policy_public_arn
+  lambda_policy_arn    = aws_iam_policy.lambda-iam-policy-selenium.arn
   lambda_role_arn      = aws_iam_role.lambda-assume-role-selenium-lambda.arn
   lambda_role_name     = aws_iam_role.lambda-assume-role-selenium-lambda.name
   ecr_image = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${data.aws_region.current.id}.amazonaws.com/${data.aws_ecr_image.service_image.repository_name}:${data.aws_ecr_image.service_image.image_tag}"
 
   env_vars = {
-    "ENVIRONMENT_NAME" = upper(var.env_name)
+    "ENVIRONMENT_NAME" = upper(var.env_name),
+    "BUCKET_NAME" = data.terraform_remote_state.stack.outputs.s3_reentry_bucket_id,
+    "MAPBOX_ACCESS_TOKEN" = local.secret_data["nextPublicMapboxAccessToken"]
   }
 }
 
