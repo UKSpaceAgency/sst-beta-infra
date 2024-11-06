0a61a35c43b9e15e07324fa17c007d6f04d2228b
thepsc-go <gregory.o@thepsc.co.uk>
2024-11-06 10:46:23 +0100

    Revert "deleted dev frontend2 (www2)"
    This reverts commit 977feaaab0caa1eea377fd9f75af4ad32f2e59a6.

---
+++ aws/envs/dev/frontend2/data.tf
@@ -0,0 +1,22 @@
+locals {
+  local_r53_domain = data.terraform_remote_state.stack.outputs.route53_domain
+}
+
+data "terraform_remote_state" "stack" {
+  backend = "s3"
+
+  config = {
+    bucket  = "uksa-mys-dev-tf-states"
+    region  = "eu-west-2"
+    key     = "dev-env-structures"
+    profile = "uksa-mys-dev-env"
+    assume_role = {
+      role_arn = "arn:aws:iam::915338536460:role/tf-power-role"
+    }
+  }
+}
+
+
+data "aws_secretsmanager_secret" "by-name" {
+  name = "${var.env_name}-frontend"
+}
---
+++ aws/envs/dev/frontend2/main.tf
@@ -0,0 +1,35 @@
+# Bucket need to be created manually
+terraform {
+  required_version = ">= 1.6.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    encrypt  = true
+    bucket   = "uksa-mys-dev-tf-states"
+    region   = "eu-west-2"
+    key      = "dev-env-structures.frontend2"
+    profile  = "uksa-mys-dev-env"
+    role_arn = "arn:aws:iam::915338536460:role/tf-power-role"
+  }
+}
+
+provider "aws" {
+  region  = "eu-west-2"
+  profile = "uksa-mys-dev-env"
+  assume_role {
+    role_arn = "arn:aws:iam::915338536460:role/tf-power-role"
+  }
+  default_tags {
+    tags = {
+      Environment = var.env_name
+      Owner       = "UKSA"
+      Project     = "MyS"
+    }
+  }
+}
+
