3898286d382abe700a48a4cb83a465a05d0e31ef
thepsc-go <gregory.o@thepsc.co.uk>
2025-01-20 10:32:45 +0100

    jobs timing changes

--- aws/envs/demo/backend/jobs_when_map.tf
+++ aws/envs/demo/backend/jobs_when_map.tf
@@ -1,6 +1,6 @@
 locals {
   ingestion_schedule = {
-    spacetrack    = "0 3/4 * * ? *"  //start at 3am UTC then repeat every 4h
+    spacetrack    = "20 3/4 * * ? *"  //start at 3am UTC then repeat every 4h
     esadiscos     = "0 4/12 * * ? *" //start at 4am UTC, repeat every cd ..12h
     notifications = "0 8-17 * * ? *" //every 1h between 8-17
   }
--- aws/envs/dev/backend/jobs_when_map.tf
+++ aws/envs/dev/backend/jobs_when_map.tf
@@ -1,6 +1,6 @@
 locals {
   ingestion_schedule = {
-    spacetrack    = "0 3/4 * * ? *"  //start at 3am UTC then repeat every 4h
+    spacetrack    = "15 3/4 * * ? *"  //start at 3am UTC then repeat every 4h
     esadiscos     = "0 6/12 * * ? *" //start at 2am UTC, repeat every 12h
     notifications = "0 8-17 * * ? *" //every 1h between 8-17
   }
