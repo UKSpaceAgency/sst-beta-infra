08d38c4af7a4596caa6ad6f189ab8ae161c02985
thepsc-go <gregory.o@thepsc.co.uk>
2025-01-30 17:35:32 +0100

    saving new FE config, + autodeploy

--- aws/envs/prod/backend/jobs_when_map.tf
+++ aws/envs/prod/backend/jobs_when_map.tf
@@ -1,6 +1,6 @@
 locals {
   ingestion_schedule = {
-    spacetrack    = "10 3/2 * * ? *"  //start at 3am UTC then repeat every 2h
+    spacetrack    = "10 3/2 * * ? *"  //start at 3:10am UTC then repeat every 2h
     esadiscos     = "0 2/12 * * ? *" //start at 2am UTC, repeat every 12h
     notifications = "0 8-17 * * ? *" //every 1h between 8-17
   }
