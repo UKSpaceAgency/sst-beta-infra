bc7caf1eb8765f81d2f587c1cedaf516b961470d
thepsc-go <gregory.o@thepsc.co.uk>
2025-11-05 17:13:25 +0100

    data-cache bucket allow read from dev and demo accounts

--- aws/tf-modules/data_cache_baseline/bucket.tf
+++ aws/tf-modules/data_cache_baseline/bucket.tf
@@ -5,3 +5,61 @@ resource "aws_s3_bucket" "data_cache_bucket" {
   force_destroy = true
 }
 
+data "aws_iam_policy_document" "data_cache_bucket_policy" {
+  statement {
+    sid = "DenyNonHttpsTraffic"
+    effect = "Deny"
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    actions = [
+      "s3:*"
+    ]
+    resources = [
+      aws_s3_bucket.data_cache_bucket.arn, "${aws_s3_bucket.data_cache_bucket.arn}/*",
+    ]
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values = [
+        "false"
+      ]
+    }
+  }
+
+  statement {
+    sid = "AllowGetFromDevAccount"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::915338536460:role/ecs-task-role-for-dev"]
+    }
+    actions = [
+      "s3:GetObject"
+    ]
+    resources = [
+      "${aws_s3_bucket.data_cache_bucket.arn}/*"
+    ]
+  }
+
+  statement {
+    sid = "AllowGetFromDemoAccount"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::469816118475:role/ecs-task-role-for-demo"]
+    }
+    actions = [
+      "s3:GetObject"
+    ]
+    resources = [
+      "${aws_s3_bucket.data_cache_bucket.arn}/*"
+    ]
+  }
+}
+
+resource "aws_s3_bucket_policy" "data_cache_bucket_policy" {
+  bucket = aws_s3_bucket.data_cache_bucket.id
+  policy = data.aws_iam_policy_document.data_cache_bucket_policy.json
+}
