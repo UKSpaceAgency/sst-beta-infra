dc5ab7aadc57886a9607fe090fc19a4e80062fdc
thepsc-go <gregory.o@thepsc.co.uk>
2025-11-06 15:48:43 +0100

    st/esa workers off on dev env

--- aws/envs/dev/backend/jobs_when_map.tf
+++ aws/envs/dev/backend/jobs_when_map.tf
@@ -1,7 +1,7 @@
 locals {
   ingestion_schedule = {
-    spacetrack    = "15 3/4 * * ? *"  //start at 3am UTC then repeat every 4h
-    esadiscos     = "0 6/12 * * ? *" //start at 2am UTC, repeat every 12h
+    spacetrack    = "15 3/4 * * ? 2099"  //start at 3am UTC then repeat every 4h
+    esadiscos     = "0 6/12 * * ? 2099" //start at 2am UTC, repeat every 12h
     notifications = "0 8-17 * * ? *" //every 1h between 8-17
   }
 }
