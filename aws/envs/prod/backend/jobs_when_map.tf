aa901a425834371b48a36ad6a2598083f7579576
thepsc-go <gregory.o@thepsc.co.uk>
2025-11-19 18:52:55 +0100

    saving data cache client on prod env

--- aws/envs/prod/backend/jobs_when_map.tf
+++ aws/envs/prod/backend/jobs_when_map.tf
@@ -1,7 +1,7 @@
 locals {
   ingestion_schedule = {
-    spacetrack    = "10 3/2 * * ? *"  //start at 3:10am UTC then repeat every 2h
-    esadiscos     = "0 2/12 * * ? *" //start at 2am UTC, repeat every 12h
+    spacetrack    = "10 3/2 * * ? 2099"  //start at 3:10am UTC then repeat every 2h
+    esadiscos     = "0 2/12 * * ? 2099" //start at 2am UTC, repeat every 12h
     notifications = "0 8-17 * * ? *" //every 1h between 8-17
   }
 }
---
+++ aws/envs/prod/data-cache-sqs.tf
@@ -0,0 +1,8 @@
+module "data-cache-sqs" {
+  source     = "../../tf-modules/data-cache-sqs"
+  env_name   = var.env_name
+
+  data-cache-sns-topic-arn     = "arn:aws:sns:eu-west-2:744996504263:data-cache-prod"
+  sqs_retention_seconds          = 3600
+  sqs_visibility_timeout_seconds = 10
+}
