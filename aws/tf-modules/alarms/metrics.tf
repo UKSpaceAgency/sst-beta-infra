64abd7dae5267ccca2577109ea70d3e5327f3aa0
Michał Kras <michal.kras@thepsc.co.uk>
2026-01-14 14:31:49 +0100

    Change cadence of alarms

--- aws/tf-modules/alarms/metrics.tf
+++ aws/tf-modules/alarms/metrics.tf
@@ -55,13 +55,13 @@ module "space_track_satellites_ingestion_finished" {
   source                 = "../alarm_metric"
   cluster_log_group_name = var.cluster_log_group_name
   env_name               = var.env_name
-  alarm_name             = "No ingestion of Satellites from Space-Track has been completed in last 3 hours"
-  alarm_description      = "No ingestion of Satellites from Space-Track has been completed in last 3 hours"
+  alarm_name             = "No ingestion of Satellites from Space-Track has been completed in last 25 hours"
+  alarm_description      = "No ingestion of Satellites from Space-Track has been completed in last 25 hours"
   metric_filter_name     = "space-track-satellites-ingestion-finished"
   metric_filter_pattern  = "%Finished pulling pack of satellites from SpaceTrack%"
   metric_name            = "satellites-ingestion-finished"
   metric_namespace       = local.space_track_namespace
-  period_in_seconds      = local.three_hours_in_seconds
+  period_in_seconds      = local.twenty_five_hrs_in_seconds
   default_statistic      = "Maximum"
 }
 
@@ -69,13 +69,13 @@ module "space_track_ingestion_finished" {
   source                 = "../alarm_metric"
   cluster_log_group_name = var.cluster_log_group_name
   env_name               = var.env_name
-  alarm_name             = "No ingestion of CDMs has been completed in the last 6.5 hours"
-  alarm_description      = "No ingestion of CDMs has been completed in the last 6.5 hours"
+  alarm_name             = "No ingestion of CDMs has been completed in the last 3 hours"
+  alarm_description      = "No ingestion of CDMs has been completed in the last 3 hours"
   metric_filter_name     = "sync_with_space_track_cdms_finished"
   metric_filter_pattern  = "%Finished pulling CDMs from Space-Track%"
   metric_name            = "ingestion-finished"
   metric_namespace       = local.space_track_namespace
-  period_in_seconds      = local.six_and_half_hours_in_seconds
+  period_in_seconds      = local.three_hours_in_seconds
   default_statistic      = "Maximum"
 }
 
