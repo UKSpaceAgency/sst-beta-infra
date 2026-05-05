256ef04717b0dd7216807f9ab052dee793e614c8
thepsc-go <gregory.o@thepsc.co.uk>
2026-05-05 19:25:41 +0200

    deployments bucket #2

--- aws/envs/demo/s3.tf
+++ aws/envs/demo/s3.tf
@@ -1,4 +1,5 @@
 module "s3" {
   source   = "../../tf-modules/bucket"
   env_name = var.env_name
+  cloudfront_deployments_arn = "arn:aws:cloudfront::469816118475:distribution/E1YGC9KRST5EYB"
 }
--- aws/envs/prod/s3.tf
+++ aws/envs/prod/s3.tf
@@ -1,4 +1,5 @@
 module "s3" {
   source   = "../../tf-modules/bucket"
   env_name = var.env_name
+  cloudfront_deployments_arn = "arn:aws:cloudfront::744996504263:distribution/EWVJN9RN9Y7NT"
 }
