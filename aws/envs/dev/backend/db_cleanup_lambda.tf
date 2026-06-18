03aaf1af1438c540073bfd66847a2350f2490b59
thepsc-go <gregory.o@thepsc.co.uk>
2026-06-18 15:07:32 +0200

    db-cleanup lambda trigger

--- aws/envs/dev/backend/db_cleanup_lambda.tf
+++ aws/envs/dev/backend/db_cleanup_lambda.tf
@@ -30,11 +30,98 @@ module "db_cleanup_lambda" {
   lambda_role_name       = aws_iam_role.lambda-assume-role-db-cleanup.name
   s3_bucket              = data.terraform_remote_state.stack.outputs.lambdas_bucket_id
   s3_key                 = "db-cleanup-${var.image_tag}.zip"
-  default_timeout        =  900
+  default_timeout        = 900
 
   env_vars = {
     "SECRET_NAME"          = "${var.env_name}-backend"
     "ENV_NAME"             = var.env_name
     "vpc_endpoint_secrets" = data.terraform_remote_state.stack.outputs.vpc_secrets_manager_endpoint_arn
   }
+}
+
+resource "aws_cloudwatch_event_rule" "db_cleanup_daily" {
+  name                = "db-cleanup-daily-${var.env_name}"
+  description         = "Run the DEV database cleanup Lambda every day at 18:00 UTC"
+  schedule_expression = "cron(0 18 * * ? *)"
+}
+
+resource "aws_cloudwatch_event_target" "db_cleanup_lambda" {
+  rule = aws_cloudwatch_event_rule.db_cleanup_daily.name
+  arn  = module.db_cleanup_lambda.vpc_lambda_arn
+
+  input = jsonencode(
+    {
+    "environment": "DEV",
+    "tasks": [
+      {
+        "table_name": "events",
+        "batch_size": 50000,
+        "delay_seconds": 1,
+        "retention_days": 365
+      },
+      {
+        "table_name": "unique_events",
+        "batch_size": 50000,
+        "delay_seconds": 1,
+        "retention_days": 365
+      },
+      {
+        "table_name": "cdms",
+        "batch_size": 50000,
+        "delay_seconds": 1,
+        "retention_days": 365
+      },
+      {
+        "table_name": "cdm_data",
+        "batch_size": 50000,
+        "delay_seconds": 1,
+        "retention_days": 365
+      },
+      {
+        "table_name": "cdm_headers",
+        "batch_size": 50000,
+        "delay_seconds": 1,
+        "retention_days": 365
+      },
+      {
+        "table_name": "cdm_metadata",
+        "batch_size": 50000,
+        "delay_seconds": 1,
+        "retention_days": 365
+      },
+      {
+        "table_name": "cdm_relative_data_metadata",
+        "batch_size": 50000,
+        "delay_seconds": 1,
+        "retention_days": 365
+      },
+      {
+        "table_name": "analyses",
+        "batch_size": 50000,
+        "delay_seconds": 1,
+        "retention_days": 365
+      },
+      {
+        "table_name": "tips",
+        "batch_size": 50000,
+        "delay_seconds": 1,
+        "retention_days": 365
+      },
+      {
+        "table_name": "notifications",
+        "batch_size": 50000,
+        "delay_seconds": 1,
+        "retention_days": 180
+      }
+    ]
+  }
+  )
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch_events_to_invoke_db_cleanup" {
+  statement_id  = "AllowExecutionFromCloudWatchEvents"
+  action        = "lambda:InvokeFunction"
+  function_name = module.db_cleanup_lambda.vpc_lambda_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.db_cleanup_daily.arn
 }
