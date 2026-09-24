9adbf7cb296b8e52d4cbe46d30511b120571e975
Michał Kras <michal.kras@thepsc.co.uk>
2026-09-24 18:51:38 +0200

    Deploy zip lambdas from the current S3 object version so overwritten keys roll out

--- aws/tf-modules/lambda_in_vpc/lambda.tf
+++ aws/tf-modules/lambda_in_vpc/lambda.tf
@@ -8,15 +8,21 @@ resource "aws_cloudwatch_log_group" "lambda_lg" {
   retention_in_days = 14
 }
 
+data "aws_s3_object" "package" {
+  bucket = var.s3_bucket
+  key    = var.s3_key
+}
+
 resource "aws_lambda_function" "vpc_lambda" {
-  function_name = var.lambda_function_name
-  architectures = ["x86_64"]
-  role          = var.lambda_role_arn
-  handler       = var.lambda_handler_name
-  s3_bucket     = var.s3_bucket
-  s3_key        = var.s3_key
-  runtime       = "python3.11"
-  timeout       = var.default_timeout
+  function_name     = var.lambda_function_name
+  architectures     = ["x86_64"]
+  role              = var.lambda_role_arn
+  handler           = var.lambda_handler_name
+  s3_bucket         = var.s3_bucket
+  s3_key            = var.s3_key
+  s3_object_version = data.aws_s3_object.package.version_id
+  runtime           = "python3.11"
+  timeout           = var.default_timeout
   environment {
     variables = var.env_vars
   }
